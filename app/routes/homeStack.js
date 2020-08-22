8dfc1626cf94aa4d71c010ff2392262f26019009
Robert Ramirez <rsft6000@gmail.com>
2020-08-22 12:26:06 -0600

    safe-area-context fix and readme

--- app/routes/homeStack.js
+++ app/routes/homeStack.js
@@ -1,4 +1,4 @@
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createAppContainer } from 'react-navigation';
 import Home from '../screens/home';
 import ImageDetails from '../screens/Images/ImageDetails';
